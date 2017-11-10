0a65f316bcd76c6df5184bb4c006fcbb76e2e55e
Þórarinn Ingi Tómasson <1thorarinn@gmail.com>
2017-11-10 15:40:09 +0000

    Test1

--- functions.php
+++ functions.php
@@ -53,3 +53,9 @@ require_once( 'library/responsive-images.php' );
 
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/class-foundationpress-protocol-relative-theme-assets.php' );
+
+
+function uselessFunction(){
+    return '';
+
+}
